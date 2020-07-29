c176eeebb41ae9d7a69ded71ef97e473c7025abe
vikiwarrior <vikrant3302@gmail.com>
2020-07-29 14:56:27 +0530

    add two more files

---
+++ LinearSearch.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+// # to remember syntax
+
+
+//   System.out.println("Test");            
+//     Scanner sc = new Scanner(System.in);   
+//     sc.close();
+//arr[i] = sc.nextInt();
+//int arr[] = new int[n];
+
+
+class LinearSearch {
+ 
+
+   
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+         int n =sc.nextInt();
+         System.out.println(""); 
+           
+           
+ 
+ 
+
+        sc.close();
+    }
+}
---
+++ hello.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+// # to remember syntax
+
+
+//System.out.println("Element not found");            
+//     Scanner sc = new Scanner(System.in);   
+//     sc.close();
+//arr[i] = sc.nextInt();
+//int arr[] = new int[n];
+
+
+class LinearSearch {
+ 
+
+   
+    public static void main(String args[]) {
+        
+
+    }
+}
